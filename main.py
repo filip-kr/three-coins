501f4ba6633bbbec0e81cc922267b2cf7bb1c0c1
Filip Krnjaković <f.krnja@gmail.com>
2023-03-11 18:55:47 +0100

    Output fix

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ if __name__ == '__main__':
     import three_coins
     import line
 
-    input('Enter your question, focus on it, and press ENTER to start tossing coins\n')
+    input('Enter your question, focus on it, and press ENTER to start tossing coins: ')
+    print('\nCoin toss round:')
 
     hexagram = []
 
@@ -12,10 +13,8 @@ if __name__ == '__main__':
         hexagram.insert(count, coin_toss_result)
 
         count += 1
-        input('Coin toss: ' + str(count) + '/6')
-        print('\nPresent:') if count == 6 else None
-
-    hexagram.reverse()
+        input(str(count) + '/6')
+        print('\nPRESENT:') if count == 6 else None
 
     for coin_toss_result in hexagram:
         print(line.draw(coin_toss_result))
