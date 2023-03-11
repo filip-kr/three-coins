0587c8dd04669a2410360ce4b24e818b976446ad
Filip Krnjaković <f.krnja@gmail.com>
2023-03-11 17:15:34 +0100

    Line number and value print

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 if __name__ == '__main__':
     import three_coins
 
-    input('Enter your question, focus on it, and press ENTER to start tossing coins')
+    input('Enter your question, focus on it, and press ENTER to start tossing coins\n')
 
-    count = 0
-    while count < 6:
-        print(three_coins.toss())
+    count = 1
+    hexagram = []
+
+    while count < 7:
+        line = three_coins.toss()
+        hexagram.insert(0, line)
+
+        print(str(count) + ' : ' + str(hexagram[0]))
+
         count += 1
+        input()
+
+    hexagram.reverse()
+    print(hexagram)
