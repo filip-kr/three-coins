c1f032b8ad13cdd7eca82c52bc02a45cd9932fa4
Filip Krnjaković <f.krnja@gmail.com>
2023-03-11 19:44:53 +0100

    Hexagram binary

--- main.py
+++ main.py
@@ -6,17 +6,27 @@ if __name__ == '__main__':
     print('\nCoin toss round:')
 
     hexagram = []
+    binary = []
 
     count = 0
     while count < 6:
         coin_toss_result = three_coins.toss()
-        hexagram.insert(count, coin_toss_result)
+        hexagram.insert(count, + coin_toss_result)
+
+        if coin_toss_result == 6 or coin_toss_result == 8:
+            binary.insert(count, '0')
+        else:
+            binary.insert(count, '1')
 
         count += 1
         input(str(count) + '/6')
-        print('\nPRESENT:') if count == 6 else None
+
+    hexagram.reverse()
+    binary = ''.join(binary)
+
+    print('\nPRESENT:')
 
     for coin_toss_result in hexagram:
         print(line.draw(coin_toss_result))
 
-    print('\n')
+    print(binary)
